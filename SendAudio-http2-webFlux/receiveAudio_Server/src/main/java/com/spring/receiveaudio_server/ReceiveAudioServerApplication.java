f90646b22c72e5595c25e7ccc445ea17744862e6
code-hyun <jh940412@gmail.com>
2024-01-12 16:46:14 +0900

    client-server

---
+++ SendAudio-http2-webFlux/receiveAudio_Server/src/main/java/com/spring/receiveaudio_server/ReceiveAudioServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.receiveaudio_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiveAudioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiveAudioServerApplication.class, args);
+	}
+
+}
